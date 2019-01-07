4170ade481d06dfcdbf3bf287fc877196958b0e6
xianbx <378434086@qq.com>
2019-01-07 17:39:50 +0800

    add antd in login.tsx

--- back-end-system/src/pages/login/login.tsx
+++ back-end-system/src/pages/login/login.tsx
@@ -1,35 +1,83 @@
+import { Button, Checkbox, Form, Icon, Input } from "antd";
 import * as React from "react";
+
 import "./login.css";
 
-// import loginBg from "src/assets/logo-bg.svg";
+export interface IProps {
+  // form: () => void;
+  getFieldDecorator: () => void;
+}
 
-class Login extends React.Component {
+class Login extends React.Component<IProps, object> {
   public render() {
+    // return (
+    //   <div className="login">
+    //     <div className="login-container">
+    //       <div className="login-main">
+    //         <div className="login-scroll">
+    //           <div className="login-tabs">
+    //             <div className="tab active">账户密码登录</div>
+    //             <div className="tab">手机号登录</div>
+    //             <div className="tabs-bar" />
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // );
+    // const { form } = this.props;
+    const { getFieldDecorator } = this.props.form;
     return (
-      <div className="login">
-        <div className="login-container">
-          <div className="login-main">
-            <div className="login-scroll">
-              <div className="login-tabs">
-                <div className="tab active">账户密码登录</div>
-                <div className="tab">手机号登录</div>
-                <div className="tabs-bar" />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <h1 className="App-title">Welcome to React</h1>
-      //   </header>
-      //   <p className="App-intro">
-      //     To get started, edit <code>src/App.tsx</code> and save to reload.
-      //   </p>
-      // </div>
+      <Form onSubmit={this.handleSubmit} className="login-form">
+        <Form.Item>
+          {getFieldDecorator("userName", {
+            rules: [{ required: true, message: "Please input your username!" }]
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="Username"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator("password", {
+            rules: [{ required: true, message: "Please input your Password!" }]
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+              type="password"
+              placeholder="Password"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator("remember", {
+            valuePropName: "checked",
+            initialValue: true
+          })(<Checkbox>Remember me</Checkbox>)}
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Log in
+          </Button>
+          Or <a href="">register now!</a>
+        </Form.Item>
+      </Form>
     );
   }
+  private handleSubmit = (e: any) => {
+    e.preventDefault();
+    this.props.form.validateFields((err: any, values: any) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+      }
+    });
+  };
 }
 
 export default Login;
