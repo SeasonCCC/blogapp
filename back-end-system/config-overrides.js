f60720f62fda3095648b72d7a66e7bff92274bba
xianbx <378434086@qq.com>
2019-02-22 11:33:38 +0800

    migrate create-react-app-typescript to Create React App

--- back-end-system/config-overrides.js
+++ back-end-system/config-overrides.js
@@ -1,25 +1,9 @@
 /* config-overrides.js */
-const tsImportPluginFactory = require('ts-import-plugin')
-const { getLoader } = require("react-app-rewired");
-
-module.exports = function override (config, env) {
-  const tsLoader = getLoader(
-    config.module.rules,
-    rule =>
-      rule.loader &&
-      typeof rule.loader === 'string' &&
-      rule.loader.includes('ts-loader')
-  );
-
-  tsLoader.options = {
-    getCustomTransformers: () => ({
-      before: [tsImportPluginFactory({
-        libraryDirectory: 'es',
-        libraryName: 'antd',
-        style: 'css',
-      })]
-    })
-  };
-
-  return config;
-}
+const { override, fixBabelImports } = require("customize-cra");
+module.exports = override(
+  fixBabelImports("import", {
+    libraryName: "antd",
+    libraryDirectory: "es",
+    style: "css"
+  })
+);
