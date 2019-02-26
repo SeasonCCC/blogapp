3a7ff0f2703833c1c3e9ae6ae0380e54590557a2
xianbx <378434086@qq.com>
2019-02-26 16:30:49 +0800

    add routes

--- back-end-system/src/pages/index/index.tsx
+++ back-end-system/src/pages/index/index.tsx
@@ -1,55 +1,121 @@
+import { Icon, Layout, Menu } from "antd";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+const SubMenu = Menu.SubMenu;
+
 import * as React from "react";
-import { Layout, Menu, Icon } from 'antd';
 
 const { Header, Sider, Content } = Layout;
 import "./index.css";
 
 class Index extends React.Component {
-  state = {
-    collapsed: false,
+  public state = {
+    collapsed: false
   };
 
-  toggle = () => {
+  public toggle = () => {
     this.setState({
-      collapsed: !this.state.collapsed,
+      collapsed: !this.state.collapsed
     });
-  }
+  };
 
-  render() {
+  public render() {
     return (
       <Layout>
         <Sider
           trigger={null}
-          collapsible
+          collapsible={true}
+          onCollapse={this.toggle}
           collapsed={this.state.collapsed}
         >
           <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1">
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultOpenKeys={["sub1", "sub2", "sub3"]}
+            defaultSelectedKeys={["1"]}
+          >
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                  <Icon type="bars" />
+                  <span>News</span>
+                </span>
+              }
+            >
+              <Menu.Item key="1">
+                <Link to="/">
+                  <Icon type="file-add" />
+                  <span>Add News</span>
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Link to="/login">
+                  <Icon type="edit" />
+                  <span>Edit News</span>
+                </Link>
+              </Menu.Item>
+            </SubMenu>
+
+            <SubMenu
+              key="sub2"
+              title={
+                <span>
+                  <Icon type="bars" />
+                  <span>Tips</span>
+                </span>
+              }
+            >
+              <Menu.Item key="3">
+                <Icon type="file-add" />
+                <span>Add Tips</span>
+              </Menu.Item>
+              <Menu.Item key="4">
+                <Icon type="edit" />
+                <span>Edit Tips</span>
+              </Menu.Item>
+            </SubMenu>
+
+            <SubMenu
+              key="sub3"
+              title={
+                <span>
+                  <Icon type="bars" />
+                  <span>Exposure</span>
+                </span>
+              }
+            >
+              <Menu.Item key="5">
+                <Icon type="file-add" />
+                <span>Add Exposure</span>
+              </Menu.Item>
+              <Menu.Item key="6">
+                <Icon type="edit" />
+                <span>Edit Exposure</span>
+              </Menu.Item>
+            </SubMenu>
+
+            <Menu.Item key="7">
               <Icon type="user" />
-              <span>nav 1</span>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Icon type="video-camera" />
-              <span>nav 2</span>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <Icon type="upload" />
-              <span>nav 3</span>
+              <span>Users</span>
             </Menu.Item>
           </Menu>
         </Sider>
         <Layout>
-          <Header style={{ background: '#fff', padding: 0 }}>
+          <Header style={{ background: "#fff", padding: 0 }}>
             <Icon
               className="trigger"
-              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+              type={this.state.collapsed ? "menu-unfold" : "menu-fold"}
               onClick={this.toggle}
             />
           </Header>
-          <Content style={{
-            margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280,
-          }}
+          <Content
+            style={{
+              background: "#fff",
+              margin: "24px 16px",
+              minHeight: 950,
+              padding: 24
+            }}
           >
             Content
           </Content>
--- back-end-system/src/router/router.ts
+++ back-end-system/src/router/router.ts
@@ -1,10 +1,27 @@
+import Exposure from "../pages/exposure/exposure";
 import Index from "../pages/index/index";
 import Login from "../pages/login/login";
+import News from "../pages/news/news";
+import Tips from "../pages/tips/tips";
 
 const routes = [
   {
     component: Index,
-    path: "/"
+    path: "/",
+    routes: [
+      {
+        component: News,
+        path: "/News",
+      },
+      {
+        component: News,
+        path: "/News",
+      },
+      {
+        component: News,
+        path: "/News",
+      }
+    ]
   },
   {
     component: Login,
