dc40c7e6ebfff6f37c3be32cb23f326187ddd761
xianbx <378434086@qq.com>
2019-04-10 17:33:36 +0800

    add users module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NewsController } from './news/news.controller';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [AppController, NewsController],
   providers: [AppService],
 })
