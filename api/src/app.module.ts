e3f83654092b9a525bc6f6e63c8eba18e338f48f
seasonCCC <378434086@qq.com>
2019-04-11 22:39:37 +0800

    add mongodb connect

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NewsController } from './news/news.controller';
-import { UsersModule } from './users/users.module';
+// import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URL)],
   controllers: [AppController, NewsController],
   providers: [AppService],
 })
--- api/src/users/users.module.ts
+++ api/src/users/users.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersController } from './users.controller';
+import { UserSchema } from './users.schema';
+import { UsersService } from './users.service';
 
-@Module({})
+@Module({
+  providers: [UsersService],
+  imports: [MongooseModule.forFeature([{ name: 'Cat', schema: UserSchema }])],
+  controllers: [UsersController],
+})
 export class UsersModule {}
