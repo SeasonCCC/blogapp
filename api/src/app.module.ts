542d9d2621e38fedefa555f7cfc92b49a66124f4
xianbx <378434086@qq.com>
2019-04-16 17:08:35 +0800

    add @nestjs/typeorm

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule  } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NewsController } from './news/news.controller';
 // import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URL)],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mongodb',
+    host: process.env.MONGO_URL,
+    username: 'season',
+    password: 'season',
+    database: 'foodapp',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true,
+  })],
   controllers: [AppController, NewsController],
   providers: [AppService],
 })
