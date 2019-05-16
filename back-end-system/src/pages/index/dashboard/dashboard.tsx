c4db95c51b1e8731daf9bc876fa6d9725ab789a6
xianbx <378434086@qq.com>
2019-05-16 09:13:55 +0800

    add ReactEcharts

--- back-end-system/src/pages/index/dashboard/dashboard.tsx
+++ back-end-system/src/pages/index/dashboard/dashboard.tsx
@@ -2,38 +2,67 @@ import { Card, Col, Row } from 'antd'
 import * as React from 'react'
 import './dashboard.module.scss'
 import NumberCard from './components/numberCard'
-import echarts from 'echarts/lib/echarts'
-import 'echarts/lib/chart/line'
+import ReactEcharts from 'echarts-for-react'
+// import echarts from 'echarts/lib/echarts'
+// import 'echarts/lib/chart/line'
 
 class Dashboard extends React.Component {
-  public componentDidMount (): void {
-    this.initPie()
-  }
-
-  public initPie (): void {
-    setTimeout(() => {
-      const option: any = {
-        xAxis: {
-          type: 'category',
-          boundaryGap: false,
-          data: ['Mon', 'Tue', 'Wed']
-        },
-        yAxis: {
-          type: 'value'
-        },
-        series: [
-          {
-            data: [820, 932, 901],
-            type: 'line',
-            areaStyle: {}
-          }
-        ]
+  private readonly option: object = {
+    title: {
+      text: '堆叠区域图'
+    },
+    tooltip: {
+      trigger: 'axis'
+    },
+    legend: {
+      data: ['邮件营销', '联盟广告', '视频广告']
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {}
       }
-
-      const chartElement: any = document.getElementById('charts')
-      let myChart = echarts.init(chartElement)
-      myChart.setOption(option)
-    }, 1000)
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
+    xAxis: [
+      {
+        type: 'category',
+        boundaryGap: false,
+        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+      }
+    ],
+    yAxis: [
+      {
+        type: 'value'
+      }
+    ],
+    series: [
+      {
+        name: '邮件营销',
+        type: 'line',
+        stack: '总量',
+        areaStyle: { normal: {} },
+        data: [120, 132, 101, 134, 90, 230, 210]
+      },
+      {
+        name: '联盟广告',
+        type: 'line',
+        stack: '总量',
+        areaStyle: { normal: {} },
+        data: [220, 182, 191, 234, 290, 330, 310]
+      },
+      {
+        name: '视频广告',
+        type: 'line',
+        stack: '总量',
+        areaStyle: { normal: {} },
+        data: [150, 232, 201, 154, 190, 330, 410]
+      }
+    ]
   }
 
   public render (): JSX.Element {
@@ -70,13 +99,11 @@ class Dashboard extends React.Component {
         <Row gutter={16} style={{ padding: '16px 0' }}>
           <Col span={16}>
             <Card title='Data Analysis' bordered={false}>
-              <div
-                id='charts'
-                ref='charts'
-                style={{
-                  width: '100%',
-                  height: '300px'
-                }}
+              <ReactEcharts
+                option={this.option}
+                style={{ height: '350px', width: '100%' }}
+                className='react_for_echarts'
+                theme='macarons'
               />
             </Card>
           </Col>
