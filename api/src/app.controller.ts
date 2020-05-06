25b960439deddd3a9b945b838fe38416cd97ed58
SeasonCCC <378434086@qq.com>
2020-05-06 22:31:21 +0800

    update dependencies

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author: Season
+ * @Date: 2019-11-26 21:34:53
+ * @LastEditTime: 2020-05-05 10:31:52
+ * @LastEditors: Season
+ * @FilePath: \api\src\app.controller.ts
+ */
 import { Controller, Get } from '@nestjs/common';
 import AppService from './app.service';
 
